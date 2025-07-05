c17f0f8c1c76b79185c8ebbfa39ae4099d2a4601
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-07-05 08:35:31 -0400

    Update TaskController.java

--- src/main/java/com/example/task_manager_api/controller/TaskController.java
+++ src/main/java/com/example/task_manager_api/controller/TaskController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5173") //allow frontend requests
+@CrossOrigin(origins = "http://localhost:5173") //allow frontend requests with react/vite
 @RestController
 @RequestMapping("/api/tasks")
 
